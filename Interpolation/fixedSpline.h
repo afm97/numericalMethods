cd4df414c097b32473b0c594adad7d493c990484
afm97 <arthurfranca97@gmail.com>
2024-06-15 21:34:38 -0300

    Implementação do método da spline cúbica fixa

---
+++ Interpolation/fixedSpline.h
@@ -0,0 +1,60 @@
+float FixedSpline(int rows, int cols, float data[rows][cols], float constants[rows][3])
+{
+    /*A função recebe o numero de linhas e colunas de uma matriz de pontos e retorna uma matriz com as constantes dos polinõmios*/
+    /*A matriz constante possui a seguinte estrutura constant[][3] = {coef.b, coef.c, coef.d}*/
+    /*A matriz A consiste na matriz principal do método*/
+
+    float A[rows][rows];
+    A[rows][rows] = zeros(rows, rows, A);
+    constants[rows][3] = zeros(rows, 3, constants);
+
+    float h_0, h_1, h_n, a_0, a_1, a_2;
+
+    h_0 = data[1][0] - data[0][0];
+    h_n = data[rows - 1][0] - data[rows - 2][0];
+
+    A[0][0] = 2 * h_0;
+    A[0][1] = h_0;
+    A[rows - 1][rows - 1] = 2 * h_n;
+    A[rows - 1][rows - 2] = h_n;
+
+    constants[0][1] = (3 / h_0) * (data[1][1] - data[0][1]) - 3 * data[0][2];
+    constants[rows - 1][1] = 3 * data[rows - 1][2] - (3 / h_n) * (data[rows - 1][1] - data[rows - 2][1]);
+
+    for (int i = 0; i < rows - 2; i++)
+    {
+        h_0 = data[i + 1][0] - data[i][0];
+        h_1 = data[i + 2][0] - data[i + 1][0];
+
+        A[i + 1][i] = h_0;
+        A[i + 1][i + 1] = 2 * (h_0 + h_1);
+        A[i + 1][i + 2] = h_1;
+
+        a_0 = data[i][1];
+        a_1 = data[i + 1][1];
+        a_2 = data[i + 2][1];
+
+        constants[i + 1][1] = (3 / h_1) * (a_2 - a_1) - (3 / h_0) * (a_1 - a_0);
+    }
+
+    /*Obtendo-se os coeficientes "c" mediante solução do sistema linear*/
+
+    gaussianElimination(rows, A, constants);
+
+    /*Calculando-se os coeficientes "b"*/
+    for (int j = 0; j < rows - 1; j++)
+    {
+        h_0 = data[j + 1][0] - data[j][0];
+        a_0 = data[j][1];
+        a_1 = data[j + 1][1];
+
+        constants[j][0] = (1 / h_0) * (a_1 - a_0) - (h_0 / 3) * (constants[j + 1][1] + 2 * constants[j][1]);
+    }
+
+    /*Calculando-se os coeficientes "d"*/
+    for (int k = 0; k < rows - 1; k++)
+    {
+        h_0 = data[k + 1][0] - data[k][0];
+        constants[k][2] = (1 / (3 * h_0)) * (constants[k + 1][1] - constants[k][1]);
+    }
+}
