ac876d51548abef30e0dbbd291aa49b07c2352b5
afm97 <arthurfranca97@gmail.com>
2024-06-25 14:08:24 -0300

    Retorna a norma máximo de uma linha de uma matriz

---
+++ NewtonRaphsonForSystems/norm.h
@@ -0,0 +1,26 @@
+#include <math.h>
+
+double norm(unsigned int numberOfRows, double currentSolutionArray[numberOfRows], double previousSolutionArray[numberOfRows])
+{
+    double maxValueOfCurrent = 0, maxValueOfPrevious = 0, normOfArray;
+
+    for (unsigned int i = 0; i < numberOfRows; i++)
+    {
+        if (fabs(currentSolutionArray[i]) > maxValueOfCurrent)
+        {
+            maxValueOfCurrent = fabs(currentSolutionArray[i]);
+        }
+    }
+
+    for (unsigned int j = 0; j < numberOfRows; j++)
+    {
+        if (fabs(previousSolutionArray[j]) > maxValueOfPrevious)
+        {
+            maxValueOfPrevious = fabs(previousSolutionArray[j]);
+        }
+    }
+
+    normOfArray = fabs(maxValueOfCurrent - maxValueOfPrevious) / maxValueOfCurrent;
+
+    return normOfArray;
+}
