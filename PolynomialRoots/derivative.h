993d3a36fadfdafb44b9988e74230082bc22691a
afm97 <arthurfranca97@gmail.com>
2024-07-22 11:45:35 -0300

    Implementação do cálculo de derivada

---
+++ PolynomialRoots/derivative.h
@@ -0,0 +1,6 @@
+float derivative_function_calc(float x0){
+    //ESTA FUNCAO CALCULA O VALOR DA DERIVADA DE UMA FUNCAO EM DETERMINADO PONTO X0
+
+    return 2*x0;
+    //return(18*pow(x0,2) - 50*x0 + 2);
+}
